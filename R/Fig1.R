17946bebd8f465be1a22f393593a250239e21726
Kelsey E Johnson <kelseyelizabethjohnson@gmail.com>
2023-08-04 11:39:20 -0500

    add code

---
+++ R/Fig1.R
@@ -0,0 +1,43 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(Hmisc)
+library(ggcorrplot)
+library(ggrepel)
+library(readxl)
+theme_set(theme_bw())
+
+setwd("~/Documents/postdoc/milk_genomics/CMV/manuscript_drafts/code_to_share/CMV_milk_genomics/R/")
+
+# load data - CMV aligned reads per sample
+# Table S1
+cmv.reads = read_excel("~/Documents/postdoc/milk_genomics/CMV/manuscript_drafts/supp_tables/CMV_milk_genomics_suppTables_v2.xlsx",
+                      sheet=1)
+
+## Figure 1B: proportion samples CMV+/-
+ggplot(cmv.reads %>% mutate(grp = "all")) + 
+  geom_bar(aes(x=grp,fill=milk.cmv.status,y=)) +
+  scale_fill_manual(values=c("orange","slateblue")) +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + xlab("") + ylab("Number of samples") +
+  scale_y_continuous(expand = c(0,0), breaks=c(0,50,100,150,200,250)) + 
+  scale_x_discrete(expand = c(0,0)) + 
+  labs(fill="")
+ggsave("../figs/fig1B.png",width=2,height=3)
+
+## Figure 1C: proportion CMV-mapped reads for CMV+ samples
+ggplot(cmv.reads %>% filter(cmv.prop>0)) + 
+  geom_histogram(aes(x=cmv.prop,y=..count..),fill="slateblue",bins=15) +
+  scale_x_log10(breaks=c(1e-7,1e-6,1e-5,1e-4)) + 
+  xlab("Proportion CMV-mapped reads") + ylab("Count of milk samples")
+ggsave("../figs/fig1C.png",width=3,height=3)
+
+## Figure 1D: density of CMV-mapped reads across CMV genome
+both.align = readRDS("../data/SMS_WGS_CMValigned_readPos.rds")
+
+ggplot(both.align %>% mutate(pos.kb = pos1/1000)) + geom_density(aes(x=pos.kb,y=..density..),bw=0.5,color="slateblue") + 
+  xlab("Position on CMV genome (kb)") + ylab("Density of CMV-mapped reads")
+ggsave("../figs/fig1D.png",width=4,height=3)
+
+
+
---
+++ R/Fig2.R
@@ -0,0 +1,55 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(Hmisc)
+library(ggcorrplot)
+library(ggrepel)
+library(readxl)
+theme_set(theme_bw())
+
+setwd("~/Documents/postdoc/milk_genomics/CMV/manuscript_drafts/code_to_share/CMV_milk_genomics/R/")
+
+## load differential gene expression results - Table S4
+table.s4 = read_excel("~/Documents/postdoc/milk_genomics/CMV/manuscript_drafts/supp_tables/CMV_milk_genomics_suppTables_v2.xlsx",
+                      sheet=4)
+
+## Figure 2A
+# calculated distribution expected P-values (uniform distr.)
+table.s4$Pexp = (1:nrow(table.s4))/nrow(table.s4)
+# add color label for sig genes
+table.s4 = table.s4 %>% mutate(lab=ifelse(padj<0.05,gene_name,NA), fdr.pass = ifelse(padj<0.05,"yes","no"))
+# make qqplot
+ggplot(table.s4) + geom_abline() + 
+  geom_point(aes(x=-log10(Pexp), y=-log10(pvalue), color=fdr.pass)) +
+  geom_text_repel(aes(x=-log10(Pexp), y=-log10(pvalue),label=lab),fontface="italic",max.overlaps = 15,box.padding = 0.1) +
+  scale_color_manual(values=c("darkgray","magenta")) +
+  theme(legend.position="None") + 
+  xlab("Expected P-value (-log10)") + ylab("Observed P-value (-log10)")
+ggsave("../figs/fig2A.png",width=4,height=4)
+
+
+## Figure 2B - volcano plot
+ggplot(table.s4) + 
+  geom_point(aes(x=log2FoldChange, y=-log10(pvalue), color=fdr.pass)) +
+  geom_text_repel(aes(x=log2FoldChange, y=-log10(pvalue),label=lab),fontface="italic") +
+  xlab("Log2 fold change in CMV+") + ylab("P-value (-log10)") +
+  scale_color_manual(values=c("darkgray","magenta")) +
+  theme(legend.position="None")
+ggsave("../figs/fig2B.png",width=4,height=4)
+
+
+## Figure 2C
+# load data - immune/epi cell expr. from Nyquist et al. vs. CMV logFC (our data)
+fig2c.dat = readRDS("../data/immune_epithelial_cellExpr_vs_CMVlogFC.rds")
+# plot
+ggplot(fig2c.dat, aes(x=log2FoldChange, y=diff)) + geom_point() +
+  geom_text_repel(aes(x=log2FoldChange,y=diff,label=GENE),fontface="italic",max.overlaps = 5,
+                  point.padding = 0.1, force=1.5) + 
+  xlab("Log2 fold change in CMV+") +
+  ylab("Difference in scaled gene expression\n(immune cells - luminal cells)")
+ggsave("../figs/fig2C.png",width=4.5,height=4)
+
+
+
+
